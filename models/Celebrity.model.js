817f465fd590352d7c886c2d0106aca8c0c67ac1
Ervin Varga <vargaervinerno0@gmail.comgit>
2022-11-15 17:57:16 +0100

    almost completed

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
 //  Add your code here
+const celebritySchema = new mongoose.Schema ({
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+})
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema)
+
+module.exports = Celebrity
